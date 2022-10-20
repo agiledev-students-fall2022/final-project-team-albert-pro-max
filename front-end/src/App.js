c4db0562d6aaa73022f8d0d40259ca31e72ea50f
Yilin <ys3719@nyu.edu>
2022-10-20 13:43:13 -0400

    Update profile

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,25 @@
+import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
-import Profile from './Profile.js';
+import CoursePage from './CoursePage';
+import ShoppingCart from './ShoppingCart';
+import Profile from './Profile';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <main className="App-main">
           <Routes>
-            {/* a route to see about us page*/}
-            <Route path="/" element={<Profile />} />
+            {/* a route to see a list of all messages */}
+            <Route path="/coursepage" element={<CoursePage id={6}/>} />
+            <Route path="/shoppingcart" element={<ShoppingCart id={6}/>} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </main>
       </Router>
     </div>
-  )
+  );
 }
-
 
 export default App;
