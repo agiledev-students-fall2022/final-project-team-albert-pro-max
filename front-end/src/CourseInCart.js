2d5e5f328c3d024350a43b288cc4463321df9ee0
ljm297 <jl10321@nyu.edu>
2022-10-21 20:23:47 -0400

    shopping cart style change

--- front-end/src/CourseInCart.js
+++ front-end/src/CourseInCart.js
@@ -1,27 +1,24 @@
 import React from 'react'
-import Box from '@material-ui/core/Box';
-import Button from './Button';
+// import Button from './Button';
 import './CourseInCart.css'
 const CourseInCart = () =>{
     return(
         <div className="CourseInCart">
-            <Box style={singleBox}>
                 Course Name
-                <Button title='Watch' id='watch'/>
-                <Button title='Show' id='show'/>
-            </Box>
+                <button id='watch'>watch</button>
+                <button id='show'>show</button>
         </div>
     )
 
 }
 export default CourseInCart
 
-const singleBox = {
-    border: 1,
-    borderColor: "black",
-    backgroundColor: "gray",
-    borderStyle: "solid",
-    padding: 15,
-    margin: 10,
+// const singleBox = {
+//     border: 1,
+//     borderColor: rgb(212, 212, 212),
+//     backgroundColor: "gray",
+//     borderStyle: "solid",
+//     padding: 15,
+//     margin: 10,
 
-};
+// };
