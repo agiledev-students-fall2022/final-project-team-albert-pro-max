462af65d90ece7995cd420d0623a8812f1fd3f18
Yilin <ys3719@nyu.edu>
2022-10-23 09:29:09 -0500

    add Popup

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -1,12 +1,15 @@
 import'./Profile.css'
 import ProfileLogo from './ProfileLogo.jpeg'
 import { Link } from 'react-router-dom'
+import Popup from './Popup'
+import { useState } from 'react'
 /**
  * A React component that represents the profile page of the app.
  * @param {*} param0 an object holding any props and a few function definitions passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
 const Profile = props => {
+  const [buttonPopup,setButtonPopup]=useState(false);
     return (
       <>
         <div class="image-txt-container">
@@ -16,11 +19,17 @@ const Profile = props => {
             </h2>
         </div>
         <p>netID@nyu.edu</p>
-        <div class='options'>
-            <button type="button">reset username<br></br></button>
-            <button type="button">reset password<br></br></button>
-            <button type="button">reset email<br></br></button>
+        <div class='pop-up'>
+          <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h3>This is a popup window</h3>
+          </Popup>
         </div>
+        <div class='options'>
+            <button class="button" onClick={()=>setButtonPopup(true)}>reset username<br></br></button>
+            <button class="button" onClick={()=>setButtonPopup(true)}>reset password<br></br></button>
+            <button class="button" onClick={()=>setButtonPopup(true)}>reset email<br></br></button>
+        </div>
+        
        
         
       </>
