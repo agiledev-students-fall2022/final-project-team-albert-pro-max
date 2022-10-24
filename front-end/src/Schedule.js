c681d997b0352df2bcb7662e82cdfd23ea69571e
86larryli <86larryli@gmail.com>
2022-10-24 17:38:13 -0400

    Added course schedule table

--- front-end/src/Schedule.js
+++ front-end/src/Schedule.js
@@ -1,18 +1,110 @@
 import './Schedule.css'
 import { useState, useEffect } from 'react'
 
-const Schedule = ({show}) => {
+const Schedule = ({ show }) => {
+    let scheduleRows = [];
     const [courseBlock, setCourseBlock] = useState("")
 
     useEffect(() => {
-        if (show) setCourseBlock("Agile")
-        else setCourseBlock("")
+        if (show) {
+            scheduleRows = [];
+
+            for (let hr = 8; hr <= 20; hr++) {
+                for (let min = 0; min <= 55; min += 5) {
+                    let scheduleRow = [];
+
+                    let hr_str, min_str;
+
+                    if (hr < 10) {
+                        hr_str = "0" + hr;
+                    } else {
+                        hr_str = "" + hr;
+                    }
+                    if (min < 10) {
+                        min_str = "0" + min;
+                    } else {
+                        min_str = "" + min;
+                    }
+
+                    if (min % 30 === 0) {
+                        scheduleRow.push(
+                            <th id={hr_str + min_str} rowSpan={6}>{hr_str}:{min_str}</th>
+                        );
+                    }
+
+                    if (hr === 12 && min === 30) {
+                        scheduleRow.push(
+                            <>
+                                <td rowSpan={15}>12:30-13:45<br />CSCI-UA 480<br />Agile Software Development</td>
+                                <td className='noshow'></td>
+                                <td rowSpan={15}>12:30-13:45<br />CSCI-UA 480<br />Agile Software Development</td>
+                            </>
+                        );
+                    }
+
+                    scheduleRows.push(
+                        <tr>{scheduleRow}</tr>
+                    );
+                }
+            }
+
+            setCourseBlock(scheduleRows);
+        } else {
+            scheduleRows = [];
+
+            for (let hr = 8; hr <= 20; hr++) {
+                for (let min = 0; min <= 55; min += 5) {
+                    let scheduleRow = [];
+
+                    let hr_str, min_str;
+
+                    if (hr < 10) {
+                        hr_str = "0" + hr;
+                    } else {
+                        hr_str = "" + hr;
+                    }
+                    if (min < 10) {
+                        min_str = "0" + min;
+                    } else {
+                        min_str = "" + min;
+                    }
+
+                    if (min % 30 === 0) {
+                        scheduleRow.push(
+                            <th id={hr_str + min_str} rowSpan={6}>{hr_str}:{min_str}</th>
+                        );
+                    }
+
+                    scheduleRows.push(
+                        <tr>{scheduleRow}</tr>
+                    );
+                }
+            }
+
+            setCourseBlock(scheduleRows);
+        }
     }, [show])
 
     return (
         <>
             <h2>Schedule</h2>
-            <>{courseBlock}</>
+            <center>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th className='weekday'>Mon</th>
+                            <th className='weekday'>Tue</th>
+                            <th className='weekday'>Wed</th>
+                            <th className='weekday'>Thu</th>
+                            <th className='weekday'>Fri</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {courseBlock}
+                    </tbody>
+                </table>
+            </center>
         </>
     )
 }
