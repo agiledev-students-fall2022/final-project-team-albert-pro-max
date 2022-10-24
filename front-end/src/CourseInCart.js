216693c1062e1db063b1e1802d96dee658d8ad97
86larryli <86larryli@gmail.com>
2022-10-24 18:24:01 -0400

    Added alert when click watch

--- front-end/src/CourseInCart.js
+++ front-end/src/CourseInCart.js
@@ -1,16 +1,21 @@
 import React from 'react'
 // import Button from './Button';
 import './CourseInCart.css'
-const CourseInCart = ({show, setShow}) =>{
+const CourseInCart = ({ show, setShow }) => {
     const handleShow = () => {
         setShow(!show);
         alert(show ? "course removed from schedule" : "course showed on schedule")
     }
-    return(
+
+    const handleWatch = () => {
+        alert("course added to watchlist\nyou will recieve an email when status changes");
+    }
+
+    return (
         <div className="CourseInCart">
-                Course Name
-                <button id='watch'>watch</button>
-                <button id='show' onClick={handleShow}>show</button>
+            Course Name
+            <button id='watch' onClick={handleWatch}>watch</button>
+            <button id='show' onClick={handleShow}>show</button>
         </div>
     )
 
