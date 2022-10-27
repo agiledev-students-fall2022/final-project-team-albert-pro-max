089e6c45777e36476fd7d808d7c9ec35ae192a5f
86larryli <86larryli@gmail.com>
2022-10-26 22:43:10 -0400

    Updated course catalog page to using mock data

--- front-end/src/Course.js
+++ front-end/src/Course.js
@@ -1,57 +1,22 @@
 import './Course.css'
 import { Link } from 'react-router-dom'
 
-const coursesList = [
-    {
-        id: 0, 
-        courseName: "Agile Software Engineering",
-        courseLoc: "Silver, Room 405",
-        courseTime: "TuTh 12:30PM - 1:45PM"
-    },
-    {
-        id: 1, 
-        courseName: "Applied Internet Technology",
-        courseLoc: "Online",
-        courseTime: "TuTh 9:30AM - 10:45AM"
-    },
-    {
-        id: 2, 
-        courseName: "Natural Language Processing",
-        courseLoc: "Online",
-        courseTime: "MoWe 9:30AM - 10:45AM"
-    },
-    {
-        id: 3, 
-        courseName: "Intro to Web Design and Computer Principles",
-        courseLoc: "CIWW 101",
-        courseTime: "MoWe 8:00AM - 9:15AM"
-    },
-    {
-        id: 4, 
-        courseName: "Database Design and Implementation",
-        courseLoc: "60FA 150",
-        courseTime: "TuTh 12:30PM - 1:45PM"
-    },
-    {
-        id: 5, 
-        courseName: "Intro to Computer Science",
-        courseLoc: "SILV 208",
-        courseTime: "MoWe 8:00AM - 9:15AM"
-    }
-
-];
+import mockCourses from './MockData/courses.json';
 
 const Course = prop => {
-    const id = prop.index
-    return(
-        <Link to ="/coursedetails">
+    const index = prop.index;
+    return (
+        <Link to={{
+            pathname: "/coursedetails",
+            search: `?id=${mockCourses[index].id}`
+        }}>
             <div className="course">
-                <div>{coursesList[id].courseName}</div>
-                <div>Loc: {coursesList[id].courseLoc}</div>
-                <div>Time: {coursesList[id].courseTime}</div>
+                <div>{mockCourses[index].course_name}</div>
+                <div>Loc: {mockCourses[index].location}</div>
+                <div>Time: {mockCourses[index].days} {mockCourses[index].times}</div>
             </div>
         </Link>
-        
+
     )
 };
 
--- front-end/src/CoursePage.js
+++ front-end/src/CoursePage.js
@@ -1,16 +1,18 @@
 import './CoursePage.css'
 import Course from './Course'
 
+import mockCourses from './MockData/courses.json';
+
 /**
  * A React component that represents the Home page of the app.
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
 const CoursePage = props => {
-    const num = props.id;
+    const num = mockCourses.length;
     let items = [];
     for (let i = 0; i < num; i++) {
-        items.push(<Course key={i} index={i}/>)
+        items.push(<Course key={i} index={i} />)
     }
     return (
         <div className='CoursePage'>
