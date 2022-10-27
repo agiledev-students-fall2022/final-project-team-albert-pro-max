870fc0a2c4c0e30b38fff5f480d28b78636d6524
Bernice55231 <zw2911@nyu.edu>
2022-10-27 00:51:06 -0400

    delete one component

--- front-end/src/EditInfo.js
+++ front-end/src/EditInfo.js
@@ -10,8 +10,6 @@ const EditInfo = (prop) =>{
                 <input type="password" id="newPassword" name="newPassword" title="New password" /><br></br>
                 <label>Confirm Password:</label><br></br>
                 <input type="password" id="confirmPassword" name="confirmPassword" title="Confirm new password" /><br></br>
-                <label>Pasword Token:</label><br></br>
-                <input type="text" id="token" name="token" title="Password Token" /><br></br>
                 <p className="form-actions">
                     <input type="submit" value="Change Password" title="Change password" />
                 </p>
