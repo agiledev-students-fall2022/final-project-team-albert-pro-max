afaa96ccd8d9784728c602028bb591a7f7039743
Rxu64 <wx2006@nyu.edu>
2022-11-05 13:02:59 -0400

    major fetch working

--- back-end/routes/course.js
+++ back-end/routes/course.js
@@ -3,22 +3,27 @@ const router = express.Router();
 const axios = require('axios');
 require("dotenv").config({ silent: true });
 
-router.get('/search', (req, res) => {
+router.get('/search', (req, res, next) => {
     // THIS IS /course/search ROUTE
     // DO YOUR MAGIC
-
+    axios
+        .get(`${process.env.API_BASE_URL+process.env.MAJOR}?count=3&key=${process.env.API_SECRET_KEY}`)
+        .then(apiResponse => res.json(apiResponse.data))
+        .catch(err => next(err));
+/*
     res.json({
         success: true,
         data: [],
         msg: "This is /course/search"
     });
+*/
 });
 
 router.get('/catalog', (req, res) => {
     // THIS IS /course/catalog ROUTE
     // DO YOUR MAGIC
     axios
-        .get(`${process.env.API_BASE_URL}?count=5&key=${process.env.API_SECRET_KEY}`)
+        .get(`${process.env.API_BASE_URL+process.env.COURSE}?count=5&key=${process.env.API_SECRET_KEY}`)
         .then(apiResponse => res.json(apiResponse.data))
         .catch(err => next(err));
 });
