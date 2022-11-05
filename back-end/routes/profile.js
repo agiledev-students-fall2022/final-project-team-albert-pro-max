e89732ceda0f25400703174da617fe38819da4d8
Yilin <ys3719@nyu.edu>
2022-11-05 01:37:20 -0400

    add route to get user information

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+require("dotenv").config({ silent: true });
 
 router.get('/', (req, res) => {
     // THIS IS /profile ROUTE
     // DO YOUR MAGIC HERE
+    
+    axios
+    .get(`${process.env.API_BASE_URL}?count=5&key=${process.env.API_SECRET_KEY}`)
+    .then(apiResponse => res.json(apiResponse.data))
+    .catch(err => next(err));
 
     res.json({
         success: true,
