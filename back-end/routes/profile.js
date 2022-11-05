07fc858b9c49e28d0ee827d7ba7320c104d1e7d3
Yilin <ys3719@nyu.edu>
2022-11-05 16:51:28 -0400

    revised runable user profile get

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -3,20 +3,13 @@ const router = express.Router();
 const axios = require('axios');
 require("dotenv").config({ silent: true });
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     // THIS IS /profile ROUTE
     // DO YOUR MAGIC HERE
-    
     axios
-    .get(`${process.env.API_BASE_URL}?count=5&key=${process.env.API_SECRET_KEY}`)
-    .then(apiResponse => res.json(apiResponse.data))
-    .catch(err => next(err));
-
-    res.json({
-        success: true,
-        data: [],
-        msg: "This is /profile"
-    });
+        .get(`https://my.api.mockaroo.com/users.json?key=835e06d0`)
+        .then(apiResponse => res.json(apiResponse.data))
+        .catch(err => next(err));
 });
 
 router.post('/update', (req, res) => {
