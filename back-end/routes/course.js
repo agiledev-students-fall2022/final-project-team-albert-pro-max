9b61a8c2375351f2f41c8c7eb9cad1d68eab586a
86larryli <86larryli@gmail.com>
2022-11-07 19:33:15 -0500

    Added course details route and unit test

--- back-end/routes/course.js
+++ back-end/routes/course.js
@@ -26,6 +26,16 @@ router.get('/catalog', (req, res) => {
 router.get('/details', (req, res) => {
     // THIS IS /course/details ROUTE
     // DO YOUR MAGIC
+    const course_id = req.query.id;
+
+    if (!course_id) {
+        res.status(400).send("Missing param: id");
+    } else {
+        axios
+            .get(`${process.env.API_COURSE_DETAILS}&id=${course_id}`)
+            .then(apiResponse => res.json(apiResponse.data))
+            .catch(err => res.send(err));
+    }
 });
 
 router.post('/details/rating', (req, res) => {
