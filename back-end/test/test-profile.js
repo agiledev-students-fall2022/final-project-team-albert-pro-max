f89dce169461aa4ae80f334c911800d9ff467412
Yilin <ys3719@nyu.edu>
2022-11-08 13:41:49 -0500

    updated unit test for profile

---
+++ back-end/test/test-profile.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const server = require("../app");
+
+describe("GET request to /profile route", () => {
+    it("it should respond with an HTTP 200 status code and an object in the response body", done => {
+        const test_id=1
+        chai
+            .request(server)
+            .get(`/profile`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("Array");
+                done();
+            })
+    })
+});
+
+server.close();
