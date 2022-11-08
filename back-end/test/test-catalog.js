2303820b3d3c27d91ac5d90553909c880b65da8e
Bernice55231 <zw2911@nyu.edu>
2022-11-08 13:13:43 -0500

    modify test

--- back-end/test/test-catalog.js
+++ back-end/test/test-catalog.js
@@ -10,14 +10,14 @@ const server = require("../app");
 describe("GET request to /course/catalog route", () => {
     it("it should respond with an HTTP 200 status code and an object in the response body", done => {
         const test_id = 1;
-
         chai
             .request(server)
             .get(`/course/catalog`)
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a("object");
-                expect(Object.keys(res.body).length).to.equal(14);
+                res.body.should.be.a("array");
+                expect(res.body.length).to.equal(5);
+                expect(Object.keys(res.body[0]).length).to.equal(14);
                 done();
             })
     })
