d380e7b2fc20652f1763afe86c88b015e0655b61
86larryli <86larryli@gmail.com>
2022-11-13 01:47:29 -0500

    Handle /cart/show route, add unit test

--- back-end/test/test-cart.js
+++ back-end/test/test-cart.js
@@ -18,14 +18,14 @@ describe("GET request to /cart route", () => {
                 done();
             })
     });
-    
+
     it("all courses returned should have boolean in_cart = true", done => {
         chai
             .request(server)
             .get(`/cart`)
             .end((err, res) => {
                 res.body.every(el => {
-                        expect(el).to.have.property('in_cart',true)
+                    expect(el).to.have.property('in_cart', true)
                 })
                 done();
             })
@@ -44,14 +44,39 @@ describe("GET request to /cart/watch route", () => {
                 done();
             })
     });
-    
+
     it("all courses returned should have boolean watch = true", done => {
         chai
             .request(server)
             .get(`/cart/watch`)
             .end((err, res) => {
                 res.body.every(el => {
-                        expect(el).to.have.property('watch',true)
+                    expect(el).to.have.property('watch', true)
+                })
+                done();
+            })
+    });
+});
+
+describe("GET request to /cart/show route", () => {
+    it("it should respond with an HTTP 200 status code and an array in the response body", done => {
+        chai
+            .request(server)
+            .get(`/cart/show`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("Array");
+                done();
+            })
+    });
+
+    it("all courses returned should have boolean show = true", done => {
+        chai
+            .request(server)
+            .get(`/cart/show`)
+            .end((err, res) => {
+                res.body.every(el => {
+                    expect(el).to.have.property('show', true);
                 })
                 done();
             })
