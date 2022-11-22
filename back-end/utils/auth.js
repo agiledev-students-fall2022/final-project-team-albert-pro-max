f062589fed1fad25ba595772f0ab4d562e7cb5cc
Bernice55231 <zw2911@nyu.edu>
2022-11-22 13:25:12 -0500

    implementing authentication in backend

---
+++ back-end/utils/auth.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose'),
+    passport = require('passport'),
+    passportJWT = require("passport-jwt"),
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt,
+    LocalStrategy = require('passport-local').Strategy;
+const User = mongoose.model("User");
+
+    
+passport.use(new LocalStrategy(User.authenticate()));
+passport.use(new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey   : process.env.JWT_SECRET
+    },
+    function (jwtPayload, cb) {
+        //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+        User.findOne({_id: jwtPayload.id})
+            .exec((err, usr) => {
+                if(err) {
+                    cb(null, false);
+                } else {
+                    if(usr) {
+                        cb(null, usr);
+                    } else {
+                        cb(null, false);
+                    }
+                    
+                }
+            })
+            
+    }
+));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
