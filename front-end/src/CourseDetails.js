4a8c17c708a74d85c9bcb0debdd76b215ebb7c55
ljm297 <jl10321@nyu.edu>
2022-11-28 23:56:09 -0500

    link db for course details page

--- front-end/src/CourseDetails.js
+++ front-end/src/CourseDetails.js
@@ -10,16 +10,16 @@ const CourseDetails = ({ added, setAdd }) => {
 
     const [courseDetails, setCourseDetails] = useState([]);
     const [recitationSections, setRecitationSections] = useState(<></>);
-
     useEffect(() => {
-        return () => {
-            axios
-                .get('http://localhost:3001/course/details?id=' + courseId)
+        console.log("front",courseId)
+        const fetchData = async () => {
+            const res = await axios
+                .get('http://localhost:3001/course/details/' + courseId)
                 .then(response => {
                     console.log(response.data);
                     setCourseDetails(response.data);
 
-                    if (response.data.recitations.length > 0) {
+                    if (response.data.hasOwnProperty('recitations')&&response.data.recitations.length > 0) {
                         const recitationTableRows = [];
 
                         for (let i = 0; i < response.data.recitations.length; i++) {
@@ -58,11 +58,12 @@ const CourseDetails = ({ added, setAdd }) => {
                         </>);
                     }
                 })
-                .catch(err => {
-                    console.log(err)
-                })
         }
-    }, []);
+        fetchData()
+        .catch(err => {
+            console.log(err)
+        })
+    }, ['http://localhost:3001/course/details/' + courseId]);
 
     const AddCourse = (tempCourse) => {
         const courseIn = added.find((courseInList) => { return courseInList.course_name === tempCourse.course_name });
@@ -75,48 +76,53 @@ const CourseDetails = ({ added, setAdd }) => {
         alert("Time Conflict with:\nCSCI-UA - 480 Natural Language Processing\nTuTh 11:00AM - 12:45PM");
     }
 
-    return (
-        <div className='CourseDetails'>
-            <h2 className="course-title">CSCI-UA - {courseDetails.course_number}<br />{courseDetails.course_name}</h2>
 
-            <div className="course-info">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Section</td>
-                            <td>{courseDetails.section_number}</td>
-                        </tr>
-                        <tr>
-                            <td>Professor</td>
-                            <td>{courseDetails.prof_first_name} {courseDetails.prof_last_name}</td>
-                        </tr>
-                        <tr>
-                            <td>Days/Times</td>
-                            <td><img className='icon-conflict' src="/prompt.svg" onClick={handleClickConflictIcon}></img>{courseDetails.days} {courseDetails.times}</td>
-                        </tr>
-                        <tr>
-                            <td>Location</td>
-                            <td>{courseDetails.location}</td>
-                        </tr>
-                        <tr>
-                            <td>Course Rating</td>
-                            <td>{courseDetails.rating} / 5.0 <button>Submit My Rating</button></td>
-                        </tr>
-                        <tr>
-                            <td>Syllabus</td>
-                            <td><a href={courseDetails.syllabus}>Link to Course Syllabus</a></td>
-                        </tr>
-                        <tr>
-                            <td>Cart</td>
-                            <td><button onClick={() => AddCourse(courseDetails)}>Add to Shopping Cart</button></td>
-                        </tr>
-                    </tbody>
-                </table>
+    console.log("frontdetailstest",courseDetails)
+    // console.log("departmentcode",course.department_code)
+    if(courseDetails.length>0){
+        return (
+            <div className='CourseDetails'>
+                <h2 className="course-title">{courseDetails[0].department_code} - {courseDetails[0].course_number}<br />{courseDetails[0].course_name}</h2>
+    
+                <div className="course-info">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Section</td>
+                                <td>{courseDetails[0].section_number}</td>
+                            </tr>
+                            <tr>
+                                <td>Professor</td>
+                                <td>{courseDetails[0].instructor} </td>
+                            </tr>
+                            <tr>
+                                <td>Days/Times</td>
+                                <td><img className='icon-conflict' src="/prompt.svg" onClick={handleClickConflictIcon}></img>{courseDetails[0].day} {courseDetails[0].time}</td>
+                            </tr>
+                            <tr>
+                                <td>Location</td>
+                                <td>{courseDetails[0].location}</td>
+                            </tr>
+                            <tr>
+                                <td>Course Rating</td>
+                                <td>{courseDetails[0].rating} / 5.0 <button>Submit My Rating</button></td>
+                            </tr>
+                            <tr>
+                                <td>Syllabus</td>
+                                <td><a href={courseDetails[0].syllabus}>Link to Course Syllabus</a></td>
+                            </tr>
+                            <tr>
+                                <td>Cart</td>
+                                <td><button onClick={() => AddCourse(courseDetails[0])}>Add to Shopping Cart</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+    
+                {recitationSections}
             </div>
-
-            {recitationSections}
-        </div>
-    )
+        )
+    }
 }
 
 // make this component available to be imported into any other file
--- front-end/src/CoursePage.js
+++ front-end/src/CoursePage.js
@@ -17,6 +17,7 @@ const CoursePage = props => {
   const info = id.split("-")
 
   const [courses, setCourses] = useState([]);
+
   useEffect(() => {
     axios
       .get('http://localhost:3001/course/catalog')
@@ -42,7 +43,7 @@ const CoursePage = props => {
             <h2>Catalog</h2>
             {courses.map((item, index) => {
               console.log(item)
-              return <Course key={index} id={item.id} course_name={item.course_name} location={item.location} day={item.day} times={item.time}/>
+              return <Course key={index} id={item._id} course_name={item.course_name} location={item.location} day={item.day} times={item.time}/>
             })}
         </div>
     )
