46148086b7f5176b4e7503d9548f71f209fd82c8
Larry Li <86larryli@gmail.com>
2022-12-01 14:45:04 -0500

    Update course and recitation schema

--- back-end/utils/db.js
+++ back-end/utils/db.js
@@ -21,31 +21,37 @@ const Course = new mongoose.Schema({
     course_number: { type: String, required: true }, // 2
     course_name: { type: String, required: true }, // Introduction to Computer Programming (No Prior Experience)
     course_description: { type: String, required: false },
-    units: { type: String, required: true }, // 4
+    units: { type: String, required: false }, // 4
     class_number: { type: String, required: true }, // 7441
     session: { type: String, required: true }, // 1 01/23/2023 - 05/08/2023
     section_number: { type: String, required: true }, // 001
     class_status: { type: String, required: true }, // Wait List (0)
     instruction_mode: { type: String, required: true }, // In-Person
     component: { type: String, required: true }, // Lecture
-    location: { type: String, required: true }, // Washington Square
-    building: { type: String, required: true }, // 19 University Place
-    room: { type: String, required: false }, // Room 102
-    day: { type: String, required: false }, // Mon,Wed
-    time: { type: String, required: false }, // 4:55 PM - 6:10 PM
-    instructor: { type: Array, required: true }, // ["Kapp, Craig", "Bhat, Atharv", "Malepati, Sachin"]
-    rating: { type: Number, required: true }, // 4.26
-    syllabus: { type: String, required: false }, // <SOME_URL>
+    location: { type: String, required: false }, // Washington Square
+    building_room: { type: String, required: false }, // 19 University Place Room 102
+    days: { type: String, required: false }, // Mon,Wed
+    times: { type: String, required: false }, // 4:55 PM - 6:10 PM
+    instructor: { type: Array, required: false }, // ["Kapp, Craig", "Bhat, Atharv", "Malepati, Sachin"]
+    rating: { type: Number, required: false }, // 4.26
+    syllabus: { type: String, required: false }, // <SOME_URL>,
+    multi_topics: { type: Number, required: false }, // 1/0
+    topic: { type: String, required: false }
 });
 
 const Recitation = new mongoose.Schema({
-    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
-    day: { type: String, required: true }, // Mon,Wed
-    time: { type: String, required: true }, // 4:55 PM - 6:10 PM
-    location: { type: String, required: true }, // Washington Square
-    building: { type: String, required: true }, // 19 University Place
-    room: { type: String, required: false }, // Room 102
-    instructor: { type: Array, required: true }, // ["Kapp, Craig"]
+    lecture_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+    class_number: { type: String, required: true }, // 7441
+    session: { type: String, required: true }, // 1 01/23/2023 - 05/08/2023
+    section_number: { type: String, required: true }, // 001
+    class_status: { type: String, required: true }, // Wait List (0)
+    instruction_mode: { type: String, required: true }, // In-Person
+    location: { type: String, required: false }, // Washington Square
+    component: { type: String, required: true }, // Lecture
+    building_room: { type: String, required: false }, // 19 University Place Room 102
+    days: { type: String, required: false }, // Mon,Wed
+    times: { type: String, required: false }, // 4:55 PM - 6:10 PM
+    instructor: { type: Array, required: false }, // ["Kapp, Craig", "Bhat, Atharv", "Malepati, Sachin"]
 });
 
 User.plugin(passportLocalMongoose);
