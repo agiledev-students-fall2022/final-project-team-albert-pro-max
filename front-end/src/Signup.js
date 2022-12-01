7bc2e0eedd1c0eaf695b36906f84cbf10cb67ff3
Bernice55231 <zw2911@nyu.edu>
2022-12-01 15:08:28 -0500

    implement task 143

--- front-end/src/Signup.js
+++ front-end/src/Signup.js
@@ -10,24 +10,20 @@ const Signup = props => {
     const [email, setEmail] = useState("")
     
     const handleClickSignup = (evt) => {
-        console.log('username: ', username)
-        console.log('email: ', email)
-        console.log('password: ', password)
-        console.log('repassword: ', repassword)
-        // setUsername(document.getElementById("username").textContent);
-        // setPassword(document.getElementById("password").textContent);
-        // setEmail(document.getElementById("email").textContent);
-        // axios.post('http://localhost:3001/register', {
-        //     username: username,
-        //     password: password,
-        //     email: email
-        // })
-        // .then(function (response) {
-        //     console.log(response);
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        // });
+        setUsername(document.getElementById("username").value);
+        setPassword(document.getElementById("password").value);
+        setEmail(document.getElementById("email").value);
+        axios.post('http://localhost:3001/register', {
+            username: username,
+            password: password,
+            email: email
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
 
     return (
@@ -37,22 +33,22 @@ const Signup = props => {
             <table>
                 <tr>
                     <th>Username: </th>
-                    <td><input type={"text"} placeholder='Type your username here...'>{{username}}</input></td>
+                    <td><input id="username" type={"text"} placeholder='Type your username here...'></input></td>
                 </tr>
 
                 <tr>
                     <th>Email: </th>
-                    <td><input type={"text"} placeholder='Type your email here...'>{email}</input></td>
+                    <td><input id="email" type={"text"} placeholder='Type your email here...'></input></td>
                 </tr>
 
                 <tr>
                     <th>Password: </th>
-                    <td><input type={"password"} placeholder='Type your password here...'>{password}</input></td>
+                    <td><input id="password" type={"password"} placeholder='Type your password here...'></input></td>
                 </tr>
 
                 <tr>
                     <th>Confirm Password: </th>
-                    <td><input type={"password"} placeholder='Type your password again...'>{repassword}</input></td>
+                    <td><input id="repassword" type={"password"} placeholder='Type your password again...'></input></td>
                 </tr>
             </table>
 
