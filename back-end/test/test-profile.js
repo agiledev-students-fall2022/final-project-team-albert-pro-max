d6766ba27fbe22c21e2db6577625b0e21c0f6456
Bernice55231 <zw2911@nyu.edu>
2022-12-07 20:34:25 -0500

    update profile-test

--- back-end/test/test-profile.js
+++ back-end/test/test-profile.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+require("dotenv").config({ silent: true });
 
 chai.should();
 chai.use(chaiHttp);
@@ -22,7 +23,7 @@ describe("GET request to /profile route", () => {
     });
 });
 
-let jwtToken;
+const jwtToken = process.env.TEST_TOKEN;
 describe("GET request to /profile/update/username route", () => {
     before(async function () {
         server = await require("../app");
@@ -31,13 +32,56 @@ describe("GET request to /profile/update/username route", () => {
     it("it should respond with an HTTP 200 status code", done => {
         chai
             .request(server)
-            .post(`/profile/update`)
-            .send({ newUsername: 'foo-bar'})
+            .post(`/profile/update/username`)
+            .set({ Authorization: `Bearer ${jwtToken}`})
+            .send({ newUsername: 'berber'})
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a("object");
                 res.body.should.have.property("success", true);
-                res.body.should.have.property("msg", "email successfully updated");
+                res.body.should.have.property("msg", "username is successfully updated");
+                done();
+            });
+    });
+});
+
+describe("GET request to /profile/update/password route", () => {
+    before(async function () {
+        server = await require("../app");
+    });
+    
+    it("it should respond with an HTTP 200 status code", done => {
+        chai
+            .request(server)
+            .post(`/profile/update/password`)
+            .set({ Authorization: `Bearer ${jwtToken}`})
+            .send({ newPassword: ''})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                res.body.should.have.property("success", false);
+                res.body.should.have.property("msg", "invalid password!");
+                done();
+            });
+    });
+});
+
+describe("GET request to /profile/update/email route", () => {
+    before(async function () {
+        server = await require("../app");
+    });
+    
+    it("it should respond with an HTTP 200 status code", done => {
+        chai
+            .request(server)
+            .post(`/profile/update/email`)
+            .set({ Authorization: `Bearer ${jwtToken}`})
+            .send({ newEmail: 'berber@gmail.com'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                res.body.should.have.property("success", true);
+                res.body.should.have.property("msg", "email is successfully updated");
                 done();
             });
     });
